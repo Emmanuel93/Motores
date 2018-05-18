c9cb098422d13a5fe14ca9292428e9768bc159cd
Emmanuel Hernandez <EmmanuelHernandez@MacBook-Pro-de-Eduardo.local>
2018-05-18 11:13:40 -0600

    [ADD] se agrega motor electrico y el adapter al motor de gasolina

---
+++ src/com/company/MotorAdapter.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class MotorAdapter {
+
+    private MotorElectrico motorElectrico;
+
+    public MotorAdapter(MotorElectrico motorElectrico){
+        this.motorElectrico = motorElectrico;
+    }
+
+}
---
+++ src/com/company/MotorElectrico.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public interface MotorElectrico {
+
+    public void Conectar();
+
+    public void Activar();
+
+    public void moverMasRapido();
+
+    public void detener();
+
+    public void desconectar();
+
+}
